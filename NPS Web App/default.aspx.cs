1cf2fcb1a8bfb920d1e98d207abc49ed56ed3031
John Pavlik <john.a.pavlik@gmail.com>
2018-03-03 15:42:09 -0500

    Allow user identity instead of just group identity

--- NPS Web App/default.aspx.cs
+++ NPS Web App/default.aspx.cs
@@ -123,7 +123,8 @@ namespace NPS_Web_App
             {
                 groups.Add(group.Translate(typeof(NTAccount)).ToString());
             }
-
+            groups.Add(HttpContext.Current.Request.LogonUserIdentity.User.Translate(typeof(NTAccount)).ToString());
+            throw new Exception(groups.Last());
             return groups;
         }
 
